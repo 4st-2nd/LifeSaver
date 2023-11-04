3452ed9592603e67404494ebb978db8781a55351
zcxzxc <zcxzxc@github.com>
2023-11-04 21:19:16 +0900

    Add to temp controller and change path of img

---
+++ src/main/java/kr/ac/wku/inntavern/LifeSaver/controller/Main_Controller.java
@@ -0,0 +1,14 @@
+package kr.ac.wku.inntavern.LifeSaver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Main_Controller {
+
+    @GetMapping("/")
+    public String showMain() {
+        return "dist/index";
+    }
+
+}
