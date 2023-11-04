d9ad0e7ff0a05755851f1ed95e0be2ec920fce6b
co2plant <jaonz6057@wku.ac.kr>
2023-11-04 20:20:38 +0900

    Initial commit

---
+++ src/main/java/kr/ac/wku/inntavern/LifeSaver/LifeSaverApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.wku.inntavern.LifeSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/wku/inntavern/LifeSaver/LifeSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.wku.inntavern.LifeSaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
